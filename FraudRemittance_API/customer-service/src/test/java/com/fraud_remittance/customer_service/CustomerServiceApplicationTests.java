16b5161671e7332e1873bc3bb7324213b558744e
akhil3kumar <akhil.03.kumar@gmail.com>
2026-07-07 23:19:57 +0530

    new commit

---
+++ FraudRemittance_API/customer-service/src/test/java/com/fraud_remittance/customer_service/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fraud_remittance.customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ FraudRemittance_API/merchant-service/src/test/java/com/fraud_remittance/merchant_service/MerchantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fraud_remittance.merchant_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
